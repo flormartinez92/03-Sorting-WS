6b98e935901c09bb05c4e07767de732772eda111
flormartinez92 <martinez.mf.92@gmail.com>
2023-07-13 18:48:44 -0300

    Desarrollo WS ejercicio sorting

---
+++ bubblesort.js
@@ -0,0 +1,14 @@
+function bubbleSort(array = []) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+      }
+    }
+  }
+  return array;
+}
+
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
